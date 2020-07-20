e3cf7e82583303b86c2f4d032bb2b893d5dc92c3
Tej <tejmpatel98@gmail.com>
2020-07-20 20:42:06 +0530

    use instructions in README to use this script

---
+++ Number_Of_Google_Searches/NumberOfGoogleSearches.py
@@ -0,0 +1,27 @@
+from bs4 import BeautifulSoup
+
+
+with open('MyActivity.html') as html_file:  # Here MyActivity.html is the file needed from Google, make sure the .html file is in the same directory as this program or alternatively give path of "MyActivity.html" file.
+	soup = BeautifulSoup(html_file, 'lxml')
+
+
+
+visited = 0
+searched = 0
+
+
+for x in soup.find_all('div', class_ = 'content-cell mdl-cell mdl-cell--6-col mdl-typography--body-1'):
+	
+	if(x.text.split()[0] == "Visited"):
+		visited += 1
+	else:
+		searched += 1
+
+
+length = len(soup.find_all('div', class_ = 'content-cell mdl-cell mdl-cell--6-col mdl-typography--body-1'))
+
+print(f'Total Activity : {length}')
+
+print(f'Total No. of Websites Visited : {visited}')
+
+print(f'Total No. of searches on Google : {searched}')
